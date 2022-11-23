97dab6e9a15d5afabe6d894afd938117267c1389
Sam Betts <sambetts@cisco.com>
2022-11-23 13:55:18 +0000

    Calculate CVSS Exploit and Impact scores for trivy CVSSs
    Trivy only provides us with the vector and the base score, the CVSS spec
    allows us to calculate the other scores from the vector so this commit
    adds functions for doing that.

--- shared/pkg/scanner/dependency_track/dependency_track.go
+++ shared/pkg/scanner/dependency_track/dependency_track.go
@@ -40,6 +40,7 @@ import (
 	"github.com/openclarity/kubeclarity/shared/pkg/scanner/dependency_track/api/client/client/vulnerability"
 	"github.com/openclarity/kubeclarity/shared/pkg/scanner/dependency_track/api/client/models"
 	"github.com/openclarity/kubeclarity/shared/pkg/utils"
+	utilsVul "github.com/openclarity/kubeclarity/shared/pkg/utils/vulnerability"
 )
 
 // nolint:gosec
@@ -360,7 +361,7 @@ func getCVSS(vul *models.Vulnerability) []scanner.CVSS {
 	}
 	if vul.CvssV3Vector != "" {
 		ret = append(ret, scanner.CVSS{
-			Version: getCVSSV3Version(vul.CvssV3Vector),
+			Version: utilsVul.GetCVSSV3VersionFromVector(vul.CvssV3Vector),
 			Vector:  vul.CvssV3Vector,
 			Metrics: scanner.CvssMetrics{
 				BaseScore:           vul.CvssV3BaseScore,
@@ -375,10 +376,4 @@ func getCVSS(vul *models.Vulnerability) []scanner.CVSS {
 
 func removeParentheses(vector string) string {
 	return strings.TrimSuffix(strings.TrimPrefix(vector, "("), ")")
-}
-
-func getCVSSV3Version(cvssV3Vector string) string {
-	// example input "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:L/I:L/A:H"
-	// expected result "3.0"
-	return strings.TrimPrefix(strings.Split(cvssV3Vector, "/")[0], "CVSS:")
 }
