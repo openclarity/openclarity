9739f3954a371a2bdb67a6b6d1067f5869c3540d
Sam Betts <sambetts@cisco.com>
2022-12-08 09:28:35 +0000

    vmclarity: Ensure scan scope can be type cast correctly by runtime providers
    ScanScope is an interface containing a pointer passed to the provider,
    the orcestrator was turning this into a pointer to an interface to a
    pointer which was preventing the provider from unwrapping it correctly.

--- runtime_scan/pkg/orchestrator/orchestrator.go
+++ runtime_scan/pkg/orchestrator/orchestrator.go
@@ -69,7 +69,7 @@ func (o *Orchestrator) Stop() {
 }
 
 func (o *Orchestrator) Scan(scanConfig *_config.ScanConfig, scanDone chan struct{}) error {
-	instances, err := o.providerClient.Discover(context.TODO(), &scanConfig.ScanScope)
+	instances, err := o.providerClient.Discover(context.TODO(), scanConfig.ScanScope)
 	if err != nil {
 		return fmt.Errorf("failed to discover instances to scan: %v", err)
 	}
