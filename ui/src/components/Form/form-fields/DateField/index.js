6ea43893447b233e37334b2e2e81b6c08536faa0
Galia Ilanov <90548502+galiail@users.noreply.github.com>
2023-01-18 13:12:51 +0200

    fix: UI components updates based on security alerts (#304)

--- ui/src/components/Form/form-fields/DateField/index.js
+++ ui/src/components/Form/form-fields/DateField/index.js
@@ -1,47 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classnames from 'classnames';
-import moment from 'moment';
 import { isNull } from 'lodash';
-import { SingleDatePicker } from 'react-dates';
-import 'react-dates/initialize';
+import DateTimePicker from 'react-datetime-picker';
 import { isEmpty } from 'lodash';
 import { useField } from 'formik';
 import Arrow from 'components/Arrow';
 import { FieldLabel, FieldError } from 'components/Form/utils';
+import { formatDateBy } from 'utils/utils';
 
-import 'react-dates/lib/css/_datepicker.css';
 import './date-field.scss';
 
 const DATE_FORMAT = 'YYYY-MM-DD';
 
 const DateField = (props) => {
-    const {label, className, tooltipText, displayFormat="MMM Do"} = props;
+    const {label, className, tooltipText, displayFormat="MMM dd"} = props;
     const [field, meta, helpers] = useField(props);
     const {name, value} = field; 
     const {setValue} = helpers;
 
-    const [focused, setFocused] = useState(false);
-
-    const formattedValue = !!value ? moment(value) : null;
+    const formattedValue = !!value ? new Date(value) : null;
 
     return (
         <div className={classnames("form-field-wrapper", "date-field-wrapper", {[className]: className})}>
             {!isEmpty(label) && <FieldLabel tooltipId={name} tooltipText={tooltipText}>{label}</FieldLabel>}
-            <SingleDatePicker
-                date={formattedValue}
-                onDateChange={date => setValue(isNull(date) ? "" : moment(date).format(DATE_FORMAT))}
-                focused={focused}
-                onFocusChange={({focused}) => setFocused(focused)}
-                id={name}
-                daySize={30}
-                numberOfMonths={1}
-                hideKeyboardShortcutsPanel={true}
-                small={true}
-                displayFormat={displayFormat}
-                navPrev={<Arrow name="left" />}
-                navNext={<Arrow name="right" />}
-                placeholder="Date"
-                readOnly={true}
+            <DateTimePicker
+                onChange={date => setValue(isNull(date) ? "" : formatDateBy(date, DATE_FORMAT))}
+                value={formattedValue}
+                className="date-field-select"
+                calendarClassName="date-field-select-calendar"
+                calendarIcon={null}
+                clearIcon={null}
+                disableClock={true}
+                format={displayFormat}
+                name={name}
+                openWidgetsOnFocus={true}
+                minDate={new Date()}
+                prevLabel={<Arrow name="left" />}
+                nextLabel={<Arrow name="right" />}
+                prev2Label={null}
+                next2Label={null}
+                minDetail="month"
+                calendarType="US"
             />
             {meta.touched && meta.error && <FieldError>{meta.error}</FieldError>}
         </div>
