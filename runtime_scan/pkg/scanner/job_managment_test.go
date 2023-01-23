e83cf6b142da3f61958ef4ec63b339102c932a5c
Idan Frimark <idanf@cisco.com>
2023-01-23 17:22:18 +0200

    vmclarity: fix createInitTargetScanStatus crash when family config is missing

--- runtime_scan/pkg/scanner/job_managment_test.go
+++ runtime_scan/pkg/scanner/job_managment_test.go
@@ -203,3 +203,381 @@ func Test_userVulnConfigToFamiliesVulnConfig(t *testing.T) {
 		})
 	}
 }
+
+func Test_getInitScanStatusVulnerabilitiesStateFromEnabled(t *testing.T) {
+	type args struct {
+		config *models.VulnerabilitiesConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want *models.TargetScanStateState
+	}{
+		{
+			name: "enabled",
+			args: args{
+				config: &models.VulnerabilitiesConfig{
+					Enabled: utils.BoolPtr(true),
+				},
+			},
+			want: stateToPointer(models.INIT),
+		},
+		{
+			name: "disabled",
+			args: args{
+				config: &models.VulnerabilitiesConfig{
+					Enabled: utils.BoolPtr(false),
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil enabled",
+			args: args{
+				config: &models.VulnerabilitiesConfig{
+					Enabled: nil,
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil config",
+			args: args{
+				config: nil,
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getInitScanStatusVulnerabilitiesStateFromEnabled(tt.args.config)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("getInitScanStatusVulnerabilitiesStateFromEnabled() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Test_getInitScanStatusSecretsStateFromEnabled(t *testing.T) {
+	type args struct {
+		config *models.SecretsConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want *models.TargetScanStateState
+	}{
+		{
+			name: "enabled",
+			args: args{
+				config: &models.SecretsConfig{
+					Enabled: utils.BoolPtr(true),
+				},
+			},
+			want: stateToPointer(models.INIT),
+		},
+		{
+			name: "disabled",
+			args: args{
+				config: &models.SecretsConfig{
+					Enabled: utils.BoolPtr(false),
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil enabled",
+			args: args{
+				config: &models.SecretsConfig{
+					Enabled: nil,
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil config",
+			args: args{
+				config: nil,
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getInitScanStatusSecretsStateFromEnabled(tt.args.config)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("getInitScanStatusSecretsStateFromEnabled() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Test_getInitScanStatusSbomStateFromEnabled(t *testing.T) {
+	type args struct {
+		config *models.SBOMConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want *models.TargetScanStateState
+	}{
+		{
+			name: "enabled",
+			args: args{
+				config: &models.SBOMConfig{
+					Enabled: utils.BoolPtr(true),
+				},
+			},
+			want: stateToPointer(models.INIT),
+		},
+		{
+			name: "disabled",
+			args: args{
+				config: &models.SBOMConfig{
+					Enabled: utils.BoolPtr(false),
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil enabled",
+			args: args{
+				config: &models.SBOMConfig{
+					Enabled: nil,
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil config",
+			args: args{
+				config: nil,
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getInitScanStatusSbomStateFromEnabled(tt.args.config)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("getInitScanStatusSbomStateFromEnabled() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Test_getInitScanStatusRootkitsStateFromEnabled(t *testing.T) {
+	type args struct {
+		config *models.RootkitsConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want *models.TargetScanStateState
+	}{
+		{
+			name: "enabled",
+			args: args{
+				config: &models.RootkitsConfig{
+					Enabled: utils.BoolPtr(true),
+				},
+			},
+			want: stateToPointer(models.INIT),
+		},
+		{
+			name: "disabled",
+			args: args{
+				config: &models.RootkitsConfig{
+					Enabled: utils.BoolPtr(false),
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil enabled",
+			args: args{
+				config: &models.RootkitsConfig{
+					Enabled: nil,
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil config",
+			args: args{
+				config: nil,
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getInitScanStatusRootkitsStateFromEnabled(tt.args.config)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("getInitScanStatusRootkitsStateFromEnabled() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Test_getInitScanStatusMisconfigurationsStateFromEnabled(t *testing.T) {
+	type args struct {
+		config *models.MisconfigurationsConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want *models.TargetScanStateState
+	}{
+		{
+			name: "enabled",
+			args: args{
+				config: &models.MisconfigurationsConfig{
+					Enabled: utils.BoolPtr(true),
+				},
+			},
+			want: stateToPointer(models.INIT),
+		},
+		{
+			name: "disabled",
+			args: args{
+				config: &models.MisconfigurationsConfig{
+					Enabled: utils.BoolPtr(false),
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil enabled",
+			args: args{
+				config: &models.MisconfigurationsConfig{
+					Enabled: nil,
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil config",
+			args: args{
+				config: nil,
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getInitScanStatusMisconfigurationsStateFromEnabled(tt.args.config)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("getInitScanStatusMisconfigurationsStateFromEnabled() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Test_getInitScanStatusMalwareStateFromEnabled(t *testing.T) {
+	type args struct {
+		config *models.MalwareConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want *models.TargetScanStateState
+	}{
+		{
+			name: "enabled",
+			args: args{
+				config: &models.MalwareConfig{
+					Enabled: utils.BoolPtr(true),
+				},
+			},
+			want: stateToPointer(models.INIT),
+		},
+		{
+			name: "disabled",
+			args: args{
+				config: &models.MalwareConfig{
+					Enabled: utils.BoolPtr(false),
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil enabled",
+			args: args{
+				config: &models.MalwareConfig{
+					Enabled: nil,
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil config",
+			args: args{
+				config: nil,
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getInitScanStatusMalwareStateFromEnabled(tt.args.config)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("getInitScanStatusMalwareStateFromEnabled() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func Test_getInitScanStatusExploitsStateFromEnabled(t *testing.T) {
+	type args struct {
+		config *models.ExploitsConfig
+	}
+	tests := []struct {
+		name string
+		args args
+		want *models.TargetScanStateState
+	}{
+		{
+			name: "enabled",
+			args: args{
+				config: &models.ExploitsConfig{
+					Enabled: utils.BoolPtr(true),
+				},
+			},
+			want: stateToPointer(models.INIT),
+		},
+		{
+			name: "disabled",
+			args: args{
+				config: &models.ExploitsConfig{
+					Enabled: utils.BoolPtr(false),
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil enabled",
+			args: args{
+				config: &models.ExploitsConfig{
+					Enabled: nil,
+				},
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+		{
+			name: "nil config",
+			args: args{
+				config: nil,
+			},
+			want: stateToPointer(models.NOTSCANNED),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := getInitScanStatusExploitsStateFromEnabled(tt.args.config)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("getInitScanStatusExploitsStateFromEnabled() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
