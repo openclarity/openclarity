2655c4e0ced60ef74c6cec068f7278b630a308fd
Sam Betts <sambetts@cisco.com>
2023-02-06 09:57:08 +0000

    kubeclarity: Move GetAuthConfigFromPullSecretPath to shared/utils and cleanup dead code
    Moves the credential functions out of the config package and into
    shared/pkg/utils/creds, and also removes all the now unneeded functions
    left behind by the change.

--- kubeclarity/shared/pkg/utils/k8s/k8s_utils.go
+++ kubeclarity/shared/pkg/utils/k8s/k8s_utils.go
@@ -16,85 +16,13 @@
 package k8s
 
 import (
-	"context"
-	"fmt"
 	"strings"
 
 	"github.com/containers/image/v5/docker/reference"
-	"github.com/google/go-containerregistry/pkg/authn"
-	authnk8s "github.com/google/go-containerregistry/pkg/authn/kubernetes"
-	"github.com/google/go-containerregistry/pkg/name"
 	log "github.com/sirupsen/logrus"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 )
 
 const MaxK8sJobName = 63
-
-func GetPodImagePullSecrets(clientset kubernetes.Interface, pod corev1.Pod) []*corev1.Secret {
-	secrets := make([]*corev1.Secret, 0, len(pod.Spec.ImagePullSecrets))
-	for _, secretName := range pod.Spec.ImagePullSecrets {
-		secret, err := clientset.CoreV1().Secrets(pod.Namespace).Get(context.TODO(), secretName.Name, metav1.GetOptions{})
-		if err != nil {
-			log.Warnf("Failed to get secret %s in namespace %s. %+v", secretName.Name, pod.Namespace, err)
-			continue
-		}
-		secrets = append(secrets, secret)
-	}
-
-	return secrets
-}
-
-func GetMatchingSecretName(secrets []*corev1.Secret, imageName string) string {
-	named, err := reference.ParseNormalizedNamed(imageName)
-	if err != nil {
-		log.Warnf("failed to normalized image name: %v", err)
-		return ""
-	}
-
-	for _, secret := range secrets {
-		authorization, err := GetAuthConfig([]corev1.Secret{*secret}, named)
-		if err != nil {
-			log.Debugf("Failed to get auth config - skipping secret: %v", err)
-			continue
-		}
-
-		if authorization.Password != "" && authorization.Username != "" {
-			log.Debugf("Matching secret was found. name=%v", secret.Name)
-			return secret.Name
-		}
-
-		// TODO: should we try to fetch the image to see that the username and password really match?
-	}
-
-	log.Debugf("No matching secret found.")
-	return ""
-}
-
-func GetAuthConfig(secrets []corev1.Secret, named reference.Named) (*authn.AuthConfig, error) {
-	keychain, err := authnk8s.NewFromPullSecrets(context.TODO(), secrets)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create keychain: %v", err)
-	}
-
-	repository, err := name.NewRepository(reference.FamiliarName(named))
-	if err != nil {
-		return nil, fmt.Errorf("failed to create repository: %v", err)
-	}
-
-	authenticator, err := keychain.Resolve(repository)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create authenticator: %v", err)
-	}
-
-	authorization, err := authenticator.Authorization()
-	if err != nil {
-		return nil, fmt.Errorf("failed to create authorization: %v", err)
-	}
-
-	return authorization, nil
-}
 
 // ParseImageHash extracts image hash from image ID
 // input: docker-pullable://gcr.io/development-infra-208909/kubeclarity@sha256:6d5d0e4065777eec8237cefac4821702a31cd5b6255483ac50c334c057ffecfa
