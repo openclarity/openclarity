4a3e513b9effa43633cd01fedeee78bb99c58a33
Masaya Aoyama (@amsy810) <amsy810@gmail.com>
2023-02-22 09:56:37 +0900

    fix test for runtime scan parallelism

--- backend/pkg/rest/runtime_scan_controller_test.go
+++ backend/pkg/rest/runtime_scan_controller_test.go
@@ -603,7 +603,7 @@ func TestServer_getScanStatusAndScanned(t *testing.T) {
 			doneApplyingToDB: true,
 		},
 	}
-	rts := runtimescanner.CreateRuntimeScanner(mockVulScanner, nil, nil)
+	rts := runtimescanner.CreateRuntimeScanner(mockVulScanner, nil, nil, 10)
 	for _, tt := range tests {
 		tt.expectVulScanner(mockVulScanner)
 		t.Run(tt.name, func(t *testing.T) {
