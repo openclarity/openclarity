d84ad3d635d9779646e73590eb29bbc03c93c0a2
Sam Betts <sambetts@cisco.com>
2023-03-07 13:03:41 +0000

    Add /findings endpoint that uses the Findings API schema

---
+++ backend/pkg/rest/findings_controller.go
@@ -0,0 +1,46 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rest
+
+import (
+	"github.com/labstack/echo/v4"
+
+	"github.com/openclarity/vmclarity/api/models"
+)
+
+func (s *ServerImpl) GetFindings(ctx echo.Context, params models.GetFindingsParams) error {
+	return nil
+}
+
+func (s *ServerImpl) GetFindingsFindingID(ctx echo.Context, findingID models.FindingID, params models.GetFindingsFindingIDParams) error {
+	return nil
+}
+
+func (s *ServerImpl) PostFindings(ctx echo.Context) error {
+	return nil
+}
+
+func (s *ServerImpl) DeleteFindingsFindingID(ctx echo.Context, findingID models.FindingID) error {
+	return nil
+}
+
+func (s *ServerImpl) PatchFindingsFindingID(ctx echo.Context, findingID models.FindingID) error {
+	return nil
+}
+
+func (s *ServerImpl) PutFindingsFindingID(ctx echo.Context, findingID models.FindingID) error {
+	return nil
+}
