cd368195cf9a043888fd4bf9bba0e6d3b75ea530
Idan Frimark <40820488+FrimIdan@users.noreply.github.com>
2023-03-28 16:42:01 +0300

    api(ui backend): findings impact dashboard widget (#193)

--- ui_backend/api/models/models.gen.go
+++ ui_backend/api/models/models.gen.go
@@ -23,6 +23,30 @@ const (
 	VULNERABILITY    FindingType = "VULNERABILITY"
 )
 
+// Defines values for MisconfigurationSeverity.
+const (
+	MisconfigurationHighSeverity   MisconfigurationSeverity = "MisconfigurationHighSeverity"
+	MisconfigurationLowSeverity    MisconfigurationSeverity = "MisconfigurationLowSeverity"
+	MisconfigurationMediumSeverity MisconfigurationSeverity = "MisconfigurationMediumSeverity"
+)
+
+// Defines values for RootkitType.
+const (
+	APPLICATION RootkitType = "APPLICATION"
+	FIRMWARE    RootkitType = "FIRMWARE"
+	KERNEL      RootkitType = "KERNEL"
+	MEMORY      RootkitType = "MEMORY"
+)
+
+// Defines values for VulnerabilitySeverity.
+const (
+	CRITICAL   VulnerabilitySeverity = "CRITICAL"
+	HIGH       VulnerabilitySeverity = "HIGH"
+	LOW        VulnerabilitySeverity = "LOW"
+	MEDIUM     VulnerabilitySeverity = "MEDIUM"
+	NEGLIGIBLE VulnerabilitySeverity = "NEGLIGIBLE"
+)
+
 // ApiResponse An object that is returned in all cases of failures.
 type ApiResponse struct {
 	Message *string `json:"message,omitempty"`
@@ -37,6 +61,22 @@ type AssetInfo struct {
 
 // AssetType defines model for AssetType.
 type AssetType string
+
+// Exploit defines model for Exploit.
+type Exploit struct {
+	CveID       *string   `json:"cveID,omitempty"`
+	Description *string   `json:"description,omitempty"`
+	Name        *string   `json:"name,omitempty"`
+	SourceDB    *string   `json:"sourceDB,omitempty"`
+	Title       *string   `json:"title,omitempty"`
+	Urls        *[]string `json:"urls"`
+}
+
+// ExploitFindingImpact defines model for ExploitFindingImpact.
+type ExploitFindingImpact struct {
+	AffectedAssetsCount *int     `json:"affectedAssetsCount,omitempty"`
+	Exploit             *Exploit `json:"exploit,omitempty"`
+}
 
 // FindingTrend Represents the total number of findings in the time slot (between startTime and endTime).
 type FindingTrend struct {
@@ -64,8 +104,84 @@ type FindingsCount struct {
 	Vulnerabilities   *int `json:"vulnerabilities,omitempty"`
 }
 
+// FindingsImpact defines model for FindingsImpact.
+type FindingsImpact struct {
+	// Exploits Top 5 exploit findings sorted by impacted assets count
+	Exploits *[]ExploitFindingImpact `json:"exploits,omitempty"`
+
+	// Malware Top 5 malware findings sorted by impacted assets count
+	Malware *[]MalwareFindingImpact `json:"malware,omitempty"`
+
+	// Misconfigurations Top 5 misconfiguration findings sorted by impacted assets count
+	Misconfigurations *[]MisconfigurationFindingImpact `json:"misconfigurations,omitempty"`
+
+	// Packages Top 5 package findings sorted by impacted assets count
+	Packages *[]PackageFindingImpact `json:"packages,omitempty"`
+
+	// Rootkits Top 5 rootkit findings sorted by impacted assets count
+	Rootkits *[]RootkitFindingImpact `json:"rootkits,omitempty"`
+
+	// Secrets Top 5 secret findings sorted by impacted assets count
+	Secrets *[]SecretFindingImpact `json:"secrets,omitempty"`
+
+	// Vulnerabilities Top 5 vulnerability findings sorted by impacted assets count
+	Vulnerabilities *[]VulnerabilityFindingImpact `json:"vulnerabilities,omitempty"`
+}
+
 // FindingsTrends List of finding trends for all finding types.
 type FindingsTrends = []FindingTrends
+
+// Malware defines model for Malware.
+type Malware struct {
+	MalwareName *string      `json:"malwareName,omitempty"`
+	MalwareType *MalwareType `json:"malwareType,omitempty"`
+
+	// Path Path of the file that contains malware
+	Path *string `json:"path,omitempty"`
+}
+
+// MalwareFindingImpact defines model for MalwareFindingImpact.
+type MalwareFindingImpact struct {
+	AffectedAssetsCount *int     `json:"affectedAssetsCount,omitempty"`
+	Malware             *Malware `json:"malware,omitempty"`
+}
+
+// MalwareType defines model for MalwareType.
+type MalwareType = string
+
+// Misconfiguration defines model for Misconfiguration.
+type Misconfiguration struct {
+	Message         *string                   `json:"message,omitempty"`
+	Remediation     *string                   `json:"remediation,omitempty"`
+	ScannedPath     *string                   `json:"scannedPath,omitempty"`
+	ScannerName     *string                   `json:"scannerName,omitempty"`
+	Severity        *MisconfigurationSeverity `json:"severity,omitempty"`
+	TestCategory    *string                   `json:"testCategory,omitempty"`
+	TestDescription *string                   `json:"testDescription,omitempty"`
+	TestID          *string                   `json:"testID,omitempty"`
+}
+
+// MisconfigurationFindingImpact defines model for MisconfigurationFindingImpact.
+type MisconfigurationFindingImpact struct {
+	AffectedAssetsCount *int              `json:"affectedAssetsCount,omitempty"`
+	Misconfiguration    *Misconfiguration `json:"misconfiguration,omitempty"`
+}
+
+// MisconfigurationSeverity defines model for MisconfigurationSeverity.
+type MisconfigurationSeverity string
+
+// Package defines model for Package.
+type Package struct {
+	Name    *string `json:"name,omitempty"`
+	Purl    *string `json:"purl,omitempty"`
+	Version *string `json:"version,omitempty"`
+}
+
+// PackageFindingImpact defines model for PackageFindingImpact.
+type PackageFindingImpact struct {
+	AffectedAssetsCount *int     `json:"affectedAssetsCount,omitempty"`
+	Exploit             *Package `json:"exploit,omitempty"`
+}
 
 // RegionFindings Total findings for a region
 type RegionFindings struct {
@@ -107,6 +223,73 @@ type RiskyAsset struct {
 	Count     *int       `json:"count,omitempty"`
 }
 
+// Rootkit defines model for Rootkit.
+type Rootkit struct {
+	// Path Path of the file that contains rootkit
+	Path        *string      `json:"path,omitempty"`
+	RootkitName *string      `json:"rootkitName,omitempty"`
+	RootkitType *RootkitType `json:"rootkitType,omitempty"`
+}
+
+// RootkitFindingImpact defines model for RootkitFindingImpact.
+type RootkitFindingImpact struct {
+	AffectedAssetsCount *int     `json:"affectedAssetsCount,omitempty"`
+	Rootkit             *Rootkit `json:"rootkit,omitempty"`
+}
+
+// RootkitType defines model for RootkitType.
+type RootkitType string
+
+// Secret defines model for Secret.
+type Secret struct {
+	EndColumn *int `json:"endColumn,omitempty"`
+	EndLine   *int `json:"endLine,omitempty"`
+
+	// FilePath Name of the file containing the secret
+	FilePath *string `json:"filePath,omitempty"`
+
+	// Fingerprint Note: this is not unique
+	Fingerprint *string `json:"fingerprint,omitempty"`
+	StartColumn *int    `json:"startColumn,omitempty"`
+	StartLine   *int    `json:"startLine,omitempty"`
+}
+
+// SecretFindingImpact defines model for SecretFindingImpact.
+type SecretFindingImpact struct {
+	AffectedAssetsCount *int    `json:"affectedAssetsCount,omitempty"`
+	Secret              *Secret `json:"secret,omitempty"`
+}
+
+// VulnerabilitiesFindingImpact defines model for VulnerabilitiesFindingImpact.
+type VulnerabilitiesFindingImpact = []VulnerabilityFindingImpact
+
+// Vulnerability defines model for Vulnerability.
+type Vulnerability struct {
+	Cvss              *[]VulnerabilityCvss   `json:"cvss"`
+	Severity          *VulnerabilitySeverity `json:"severity,omitempty"`
+	VulnerabilityName *string                `json:"vulnerabilityName,omitempty"`
+}
+
+// VulnerabilityCvss defines model for VulnerabilityCvss.
+type VulnerabilityCvss struct {
+	Metrics *VulnerabilityCvssMetrics `json:"metrics,omitempty"`
+	Vector  *string                   `json:"vector,omitempty"`
+	Version *string                   `json:"version,omitempty"`
+}
+
+// VulnerabilityCvssMetrics defines model for VulnerabilityCvssMetrics.
+type VulnerabilityCvssMetrics struct {
+	BaseScore           *float32 `json:"baseScore,omitempty"`
+	ExploitabilityScore *float32 `json:"exploitabilityScore,omitempty"`
+	ImpactScore         *float32 `json:"impactScore,omitempty"`
+}
+
+// VulnerabilityFindingImpact defines model for VulnerabilityFindingImpact.
+type VulnerabilityFindingImpact struct {
+	AffectedAssetsCount *int           `json:"affectedAssetsCount,omitempty"`
+	Vulnerability       *Vulnerability `json:"vulnerability,omitempty"`
+}
+
 // VulnerabilityRiskyAsset Total number of vulnerability findings for an asset
 type VulnerabilityRiskyAsset struct {
 	AssetInfo                      *AssetInfo `json:"assetInfo,omitempty"`
@@ -116,6 +299,9 @@ type VulnerabilityRiskyAsset struct {
 	MediumVulnerabilitiesCount     *int       `json:"mediumVulnerabilitiesCount,omitempty"`
 	NegligibleVulnerabilitiesCount *int       `json:"negligibleVulnerabilitiesCount,omitempty"`
 }
+
+// VulnerabilitySeverity defines model for VulnerabilitySeverity.
+type VulnerabilitySeverity string
 
 // EndTime defines model for endTime.
 type EndTime = time.Time
