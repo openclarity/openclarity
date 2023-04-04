91214ea85098968494e5fca10b33f76def1cf7d1
Galia Ilanov <90548502+galiail@users.noreply.github.com>
2023-04-04 11:51:38 +0300

    save table sort (vmclarity#234)
    ## Description
    
    UI - save table sort
    
    ## Type of Change
    
    [ ] Bug Fix  
    [x] New Feature  
    [ ] Breaking Change  
    [ ] Refactor  
    [ ] Documentation  
    [ ] Other (please describe)

--- ui/src/components/TablePage/index.js
+++ ui/src/components/TablePage/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { isEmpty } from 'lodash';
 import ContentContainer from 'components/ContentContainer';
 import Table from 'components/Table';
 import SystemFilterBanner from 'components/SystemFiltersBanner';
 import { toCapitalized, BoldText } from 'utils/utils';
-import { useFilterState, useFilterDispatch, resetSystemFilters, setPage } from 'context/FiltersProvider';
+import { useFilterState, useFilterDispatch, resetSystemFilters, setPage, setSort } from 'context/FiltersProvider';
 
-const TablePage = ({tableTitle, filterType, systemFilterType, filters, expand, select, withMargin, ...tableProps}) => {
+const TablePage = ({tableTitle, filterType, systemFilterType, filters, expand, select, withMargin, defaultSortBy: initialSortBy, ...tableProps}) => {
     const navigate = useNavigate();
     const {pathname} = useLocation();
 
     const filtersState = useFilterState();
     const {systemFilters} = filtersState[systemFilterType || filterType];
-    const {selectedPageIndex} = filtersState[filterType];
+    const {selectedPageIndex, tableSort} = filtersState[filterType];
     const filtersDispatch = useFilterDispatch();
     
     const {name: systemFilterName, suffix: systemSuffix, backPath: systemFilterBackPath, filter: systemFilter, customDisplay} = systemFilters;
@@ -46,6 +47,8 @@ const TablePage = ({tableTitle, filterType, systemFilterType, filters, expand, s
                     onLineClick={({id}) => navigate(`${pathname}/${id}`)}
                     defaultPageIndex={selectedPageIndex}
                     onPageChange={pageIndex => setPage(filtersDispatch, {type: filterType, pageIndex})}
+                    defaultSortBy={isEmpty(tableSort) ? initialSortBy : tableSort}
+                    onSortChnage={tableSort => setSort(filtersDispatch, {type: filterType, tableSort})}
                     {...tableProps}
                 />
             </ContentContainer>
--- ui/src/context/FiltersProvider.js
+++ ui/src/context/FiltersProvider.js
@@ -3,7 +3,6 @@ import { create } from './utils';
 export const FILTER_TYPES = {
     ASSETS: "ASSETS",
     ASSET_SCANS: "ASSET_SCANS",
-    PACKAGES: "PACKAGES",
     SCANS: "SCANS",
     SCAN_CONFIGURATIONS: "SCAN_CONFIGURATIONS",
     FINDINGS_GENERAL: "FINDINGS_GENERAL",
@@ -21,7 +20,8 @@ const initialState = Object.keys(FILTER_TYPES).reduce((acc, curr) => ({
     [curr]: {
         tableFilters: [],
         systemFilters: {},
-        selectedPageIndex: 0
+        selectedPageIndex: 0,
+        tableSort: {}
     }
 }), {});
 
@@ -29,6 +29,7 @@ const FITLER_ACTIONS = {
     SET_TABLE_FILTERS_BY_KEY: "SET_TABLE_FILTERS_BY_KEY",
     SET_SYSTEM_FILTERS_BY_KEY: "SET_SYSTEM_FILTERS_BY_KEY",
     SET_TABLE_PAGE_BY_KEY: "SET_TABLE_PAGE_BY_KEY",
+    SET_TABLE_SORT_BY_KEY: "SET_TABLE_SORT_BY_KEY",
     RESET_ALL_FILTERS: "RESET_ALL_FILTERS",
     RESET_FILTERS_BY_KEY: "RESET_FILTERS_BY_KEY"
 }
@@ -71,11 +72,25 @@ const reducer = (state, action) => {
                 }
             };
         }
-        case FITLER_ACTIONS.RESET_ALL_FILTERS: {
+        case FITLER_ACTIONS.SET_TABLE_SORT_BY_KEY: {
+            const {filterType, tableSort} = action.payload;
+
             return {
                 ...state,
-                ...initialState
+                [filterType]: {
+                    ...state[filterType],
+                    tableSort
+                }
             };
+        }
+        case FITLER_ACTIONS.RESET_ALL_FILTERS: {
+            return Object.keys(initialState).reduce((acc, curr) => ({
+                ...acc,
+                [curr]: {
+                    ...initialState[curr],
+                    tableSort: state[curr].tableSort
+                }
+            }), {});
         }
         case FITLER_ACTIONS.RESET_FILTERS_BY_KEY: {
             const {filterTypes} = action.payload;
@@ -84,7 +99,10 @@ const reducer = (state, action) => {
                 ...state,
                 ...filterTypes.reduce((acc, curr) => ({
                     ...acc,
-                    [curr]: {...initialState[curr]}
+                    [curr]: {
+                        ...initialState[curr],
+                        tableSort: state[curr].tableSort
+                    }
                 }), {})
             };
         }
@@ -100,6 +118,7 @@ const setFilters = (dispatch, {type, filters, isSystem=false}) => dispatch({
     payload: {filterType: type, filterData: filters}
 });
 const setPage = (dispatch, {type, pageIndex}) => dispatch({type: FITLER_ACTIONS.SET_TABLE_PAGE_BY_KEY, payload: {filterType: type, pageIndex}});
+const setSort = (dispatch, {type, tableSort}) => dispatch({type: FITLER_ACTIONS.SET_TABLE_SORT_BY_KEY, payload: {filterType: type, tableSort}});
 const resetAllFilters = (dispatch) => dispatch({type: FITLER_ACTIONS.RESET_ALL_FILTERS});
 const resetFilters = (dispatch, filterTypes) => dispatch({type: FITLER_ACTIONS.RESET_FILTERS_BY_KEY, payload: {filterTypes}});
 const resetSystemFilters = (dispatch, type) => setFilters(dispatch, {type, filters: {}, isSystem: true})
@@ -110,6 +129,7 @@ export {
     useFilterDispatch,
     setFilters,
     setPage,
+    setSort,
     resetAllFilters,
     resetFilters,
     resetSystemFilters
