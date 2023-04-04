756725a9e326cffabd1d86b47cf2f093d3808d17
Galia Ilanov <90548502+galiail@users.noreply.github.com>
2023-04-04 11:51:38 +0300

    save table sort (#234)
    ## Description
    
    UI - save table sort
    
    ## Type of Change
    
    [ ] Bug Fix  
    [x] New Feature  
    [ ] Breaking Change  
    [ ] Refactor  
    [ ] Documentation  
    [ ] Other (please describe)

--- ui/src/layout/detail-displays/Findings/FindingsSystemFilterLinks/index.js
+++ ui/src/layout/detail-displays/Findings/FindingsSystemFilterLinks/index.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Button from 'components/Button';
 import Icon from 'components/Icon';
 import { FINDINGS_MAPPING, VULNERABIITY_FINDINGS_ITEM } from 'utils/systemConsts';
+import { formatNumber } from 'utils/utils';
 import { getFindingsAbsolutePathByFindingType } from 'layout/Findings';
 
 import './findings-system-filter-links.scss';
@@ -23,8 +24,8 @@ const FindingsSystemFilterLinks = ({totalVulnerabilitiesCount, findingsSummary})
         <div className="findings-system-filters-title">See other findings:</div>
         {
             [VULNERABIITY_FINDINGS_ITEM, ...Object.values(FINDINGS_MAPPING)].map(({value, totalKey, title, icon}) => {
-                const LinkTitle = VULNERABIITY_FINDINGS_ITEM.value === value ? `${totalVulnerabilitiesCount} ${title}` :
-                    `${!!findingsSummary ? (findingsSummary[totalKey] || 0) : 0} ${title}`;
+                const LinkTitle = VULNERABIITY_FINDINGS_ITEM.value === value ? `${formatNumber(totalVulnerabilitiesCount)} ${title}` :
+                    `${!!findingsSummary ? (formatNumber(findingsSummary[totalKey] || 0)) : 0} ${title}`;
 
                 return (
                     <FindingFilterLink key={value} icon={icon} title={LinkTitle} appRoute={getFindingsAbsolutePathByFindingType(value)} />
