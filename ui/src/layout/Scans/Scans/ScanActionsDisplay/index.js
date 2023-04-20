cdc478ff5a7dbdb6d04b56aa84b498b739835df3
Galia Ilanov <90548502+galiail@users.noreply.github.com>
2023-04-20 19:29:50 +0300

    UI - stop scan (#260)
    Stop scan button in the scan table and details

--- ui/src/layout/Scans/Scans/ScanActionsDisplay/index.js
+++ ui/src/layout/Scans/Scans/ScanActionsDisplay/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useFetch, FETCH_METHODS, usePrevious } from 'hooks';
 import { ICON_NAMES } from 'components/Icon';
 import IconWithTooltip from 'components/IconWithTooltip';
+import { SCAN_STATES } from 'components/ScanProgressBar';
+import { APIS } from 'utils/systemConsts';
 
 import './scan-actions-display.scss';
 
-const ScanActionsDisplay = ({data}) => {
-    const {id} = data;
+const ScanActionsDisplay = ({data, onUpdate}) => {
+    const {id, state} = data;
 
+    const [{loading, error}, fetchScan] = useFetch(APIS.SCANS, {loadOnMount: false});
+	const prevLoading = usePrevious(loading);
+
+    useEffect(() => {
+        if (prevLoading && !loading && !error && !!onUpdate) {
+            onUpdate();
+        }
+    }, [loading, prevLoading, error, onUpdate])
+
+    if ([SCAN_STATES.Done.state, SCAN_STATES.Failed.state, SCAN_STATES.Aborted.state].includes(state) || loading) {
+        return null;
+    }
+    
     return (
         <div className="scan-actions-display">
             <IconWithTooltip
@@ -17,7 +33,11 @@ const ScanActionsDisplay = ({data}) => {
                     event.stopPropagation();
                     event.preventDefault();
                     
-                    console.log("stop scan");
+                    fetchScan({
+                        method: FETCH_METHODS.PATCH,
+                        submitData: {state: SCAN_STATES.Aborted.state},
+                        formatUrl: url => `${url}/${id}`
+                    });
                 }}
             />
         </div>
