7a9fc9f11b3b54eb440bb83d7846427504224646
Peter Balogh <p.balogh.sa@gmail.com>
2023-05-24 13:31:15 +0200

    feat(api): add delete for application resources
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- api/generate.sh
+++ api/generate.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 alias goswagger="docker run --rm -it --user $(id -u):$(id -g) -e GOPATH=$GOPATH:/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger:v0.27.0"
 
 cp server/restapi/configure_kube_clarity_a_p_is.go /tmp/configure_kube_clarity_a_p_is.go
+cp client/client/kube_clarity_a_p_is_client.go /tmp/kube_clarity_a_p_is_client.go
 rm -rf server/*
 rm -rf client/*
 goswagger generate server -f swagger.yaml -t ./server
 goswagger generate client -f swagger.yaml -t ./client
 cp /tmp/configure_kube_clarity_a_p_is.go server/restapi/configure_kube_clarity_a_p_is.go
+cp /tmp/kube_clarity_a_p_is_client.go client/client/kube_clarity_a_p_is_client.go
