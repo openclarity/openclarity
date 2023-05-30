e5fff50e538718cf8cad7fe1bdf00be8b30e5b47
Galia Ilanov <90548502+galiail@users.noreply.github.com>
2023-05-30 14:15:53 +0300

    UI - filters in URL support (#438)

--- ui/src/context/FiltersProvider.js
+++ ui/src/context/FiltersProvider.js
@@ -1,3 +1,4 @@
+import { isArray, isObject } from 'lodash';
 import { create } from './utils';
 
 export const FILTER_TYPES = {
@@ -17,15 +18,18 @@ const initialState = {
             selectedPageIndex: 0
         }
     }), {}),
-    currentRuntimeScan: null
+    currentRuntimeScan: null,
+    initialized: false
 }
 
 const FITLER_ACTIONS = {
     SET_TABLE_FILTERS_BY_KEY: "SET_TABLE_FILTERS_BY_KEY",
     SET_SYSTEM_FILTERS_BY_KEY: "SET_SYSTEM_FILTERS_BY_KEY",
     SET_TABLE_PAGE_BY_KEY: "SET_TABLE_PAGE_BY_KEY",
+    SET_RUNTIME_SCAN_FILTER: "SET_RUNTIME_SCAN_FILTER",
     RESET_ALL_FILTERS: "RESET_ALL_FILTERS",
-    RESET_FILTERS_BY_KEY: "RESET_FILTERS_BY_KEY"
+    RESET_FILTERS_BY_KEY: "RESET_FILTERS_BY_KEY",
+    INITIALIZE_FILTERS: "INITIALIZE_FILTERS"
 }
 
 const reducer = (state, action) => {
@@ -66,6 +70,12 @@ const reducer = (state, action) => {
                 }
             };
         }
+        case FITLER_ACTIONS.SET_RUNTIME_SCAN_FILTER: {
+            return {
+                ...state,
+                currentRuntimeScan: action.payload
+            };
+        }
         case FITLER_ACTIONS.RESET_ALL_FILTERS: {
             return {
                 ...state,
@@ -82,6 +92,29 @@ const reducer = (state, action) => {
                 }
             };
         }
+        case FITLER_ACTIONS.INITIALIZE_FILTERS: {
+            const {filterType, tableFilters, systemFilters, currentRuntimeScan} = action.payload;
+
+            if (!Object.values(FILTER_TYPES).includes(filterType) || !isArray(tableFilters || {}) || !isObject(systemFilters || {})
+                || !isObject(currentRuntimeScan || {})) {
+                return {
+                    ...state,
+                    initialized: true
+                }
+            }
+
+            return {
+                ...state,
+                [filterType]: {
+                    ...state[filterType],
+                    tableFilters,
+                    systemFilters,
+                    selectedPageIndex: 0
+                },
+                initialized: true,
+                currentRuntimeScan
+            };
+        }
         default:
             return state;
     }
@@ -93,18 +126,25 @@ const setFilters = (dispatch, {type, filters, isSystem=false}) => dispatch({
     type: isSystem ? FITLER_ACTIONS.SET_SYSTEM_FILTERS_BY_KEY : FITLER_ACTIONS.SET_TABLE_FILTERS_BY_KEY,
     payload: {filterType: type, filterData: filters}
 });
+const setRuntimeScanFilter = (dispatch, filters) => dispatch({
+    type: FITLER_ACTIONS.SET_RUNTIME_SCAN_FILTER,
+    payload: filters
+});
 const setPage = (dispatch, {type, pageIndex}) => dispatch({type: FITLER_ACTIONS.SET_TABLE_PAGE_BY_KEY, payload: {filterType: type, pageIndex}});
 const resetAllFilters = (dispatch) => dispatch({type: FITLER_ACTIONS.RESET_ALL_FILTERS});
 const resetFilters = (dispatch, filterType) => dispatch({type: FITLER_ACTIONS.RESET_FILTERS_BY_KEY, payload: {filterType}});
-const resetSystemFilters = (dispatch, type) => setFilters(dispatch, {type, filters: {}, isSystem: true})
+const resetSystemFilters = (dispatch, type) => setFilters(dispatch, {type, filters: {}, isSystem: true});
+const initializeFilters = (dispatch, filtersData) => dispatch({type: FITLER_ACTIONS.INITIALIZE_FILTERS, payload: filtersData});
 
 export {
     FiltersProvider,
     useFilterState,
     useFilterDispatch,
     setFilters,
+    setRuntimeScanFilter,
     setPage,
     resetAllFilters,
     resetFilters,
-    resetSystemFilters
+    resetSystemFilters,
+    initializeFilters
 };
