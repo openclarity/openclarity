0649095f8a428a27544befd8b8d32c3a13ff6f25
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2023-06-09 18:45:35 +0200

    refactor: rm UTC() unnecessary conversion (#387)

--- runtime_scan/pkg/orchestrator/scanresultwatcher/watcher.go
+++ runtime_scan/pkg/orchestrator/scanresultwatcher/watcher.go
@@ -217,7 +217,7 @@ func (w *Watcher) reconcilePending(ctx context.Context, scanResult *models.Targe
 	}
 
 	scanResult.Status.General.State = utils.PointerTo(models.TargetScanStateStateScheduled)
-	scanResult.Status.General.LastTransitionTime = utils.PointerTo(time.Now().UTC())
+	scanResult.Status.General.LastTransitionTime = utils.PointerTo(time.Now())
 
 	scanResultPatch := models.TargetScanResult{
 		Status: scanResult.Status,
@@ -274,17 +274,17 @@ func (w *Watcher) reconcileScheduled(ctx context.Context, scanResult *models.Tar
 	case errors.As(err, &fatalError):
 		scanResult.Status.General.State = utils.PointerTo(models.TargetScanStateStateDone)
 		scanResult.Status.General.Errors = utils.PointerTo([]string{fatalError.Error()})
-		scanResult.Status.General.LastTransitionTime = utils.PointerTo(time.Now().UTC())
+		scanResult.Status.General.LastTransitionTime = utils.PointerTo(time.Now())
 	case errors.As(err, &retryableError):
 		// nolint:wrapcheck
 		return common.NewRequeueAfterError(retryableError.RetryAfter(), retryableError.Error())
 	case err != nil:
 		scanResult.Status.General.State = utils.PointerTo(models.TargetScanStateStateDone)
 		scanResult.Status.General.Errors = utils.PointerTo(utils.UnwrapErrorStrings(err))
-		scanResult.Status.General.LastTransitionTime = utils.PointerTo(time.Now().UTC())
+		scanResult.Status.General.LastTransitionTime = utils.PointerTo(time.Now())
 	default:
 		scanResult.Status.General.State = utils.PointerTo(models.TargetScanStateStateReadyToScan)
-		scanResult.Status.General.LastTransitionTime = utils.PointerTo(time.Now().UTC())
+		scanResult.Status.General.LastTransitionTime = utils.PointerTo(time.Now())
 	}
 
 	scanResultPatch := models.TargetScanResult{
