3e18cea741c82ba0a6c09533f632b134b0e6637a
Galia Ilanov <90548502+galiail@users.noreply.github.com>
2023-07-16 14:01:53 +0300

    vmclarity: UI terminated filter (vmclarity#466)
    * add terminated filter
    
    * add demo data
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Alexei Kravtsov <alexeik@cisco.com>

--- ui/src/layout/Assets/AssetsTable.js
+++ ui/src/layout/Assets/AssetsTable.js
@@ -1,16 +1,39 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect, useCallback } from 'react';
+import { isUndefined } from 'lodash';
 import TablePage from 'components/TablePage';
 import ExpandableList from 'components/ExpandableList';
+import ToggleButton from 'components/ToggleButton';
+import Loader from 'components/Loader';
 import { APIS } from 'utils/systemConsts';
 import { getFindingsColumnsConfigList, getVulnerabilitiesColumnConfigItem, getAssetColumnsFiltersConfig,
     findingsColumnsFiltersConfig, vulnerabilitiesCountersColumnsFiltersConfig, formatTagsToStringsList, formatDate} from 'utils/utils';
-import { FILTER_TYPES } from 'context/FiltersProvider';
+import { useFilterDispatch, useFilterState, setFilters, FILTER_TYPES } from 'context/FiltersProvider';
 
 const TABLE_TITLE = "assets";
 
 const LOCATION_SORT_IDS = ["assetInfo.location"];
 
+const ASSETS_FILTER_TYPE = FILTER_TYPES.ASSETS;
+
 const AssetsTable = () => {
+    const filtersDispatch = useFilterDispatch();
+    const filtersState = useFilterState();
+
+    const {customFilters} = filtersState[ASSETS_FILTER_TYPE];
+    const {hideTerminated} = customFilters;
+
+    const setHideTerminated = useCallback(hideTerminated => setFilters(filtersDispatch, {
+        type: ASSETS_FILTER_TYPE,
+        filters: {hideTerminated},
+        isCustom: true
+    }), [filtersDispatch]);
+    
+    useEffect(() => {
+        if (isUndefined(hideTerminated)) {
+            setHideTerminated(true);
+        }
+    }, [hideTerminated, setHideTerminated]);
+
     const columns = useMemo(() => [
         {
             Header: "Name",
@@ -49,31 +72,41 @@ const AssetsTable = () => {
             sortIds: ["lastSeen"],
             accessor: original => formatDate(original.lastSeen)
         },
-        {
+        ...(hideTerminated ? [] : [{
             Header: "Terminated On",
             id: "terminatedOn",
             sortIds: ["terminatedOn"],
             accessor: original => formatDate(original?.terminatedOn)
-        },
+        }]),
         getVulnerabilitiesColumnConfigItem(TABLE_TITLE),
         ...getFindingsColumnsConfigList(TABLE_TITLE)
-    ], []);
+    ], [hideTerminated]);
     
+    if (isUndefined(hideTerminated)) {
+        return <Loader />;
+    }
+
     return (
-        <TablePage
-            columns={columns}
-            url={APIS.ASSETS}
-            select="id,assetInfo,summary,lastSeen,terminatedOn"
-            tableTitle={TABLE_TITLE}
-            filterType={FILTER_TYPES.ASSETS}
-            filtersConfig={[
-                ...getAssetColumnsFiltersConfig(),
-                ...vulnerabilitiesCountersColumnsFiltersConfig,
-                ...findingsColumnsFiltersConfig
-            ]}
-            defaultSortBy={{sortIds: ["lastSeen", "terminatedOn"], desc: true}}
-            withMargin
-        />
+        <div style={{position: "relative"}}>
+            <div style={{position: "absolute", top: 0, right: "30px", zIndex: 1, display: "flex", alignItems: "center"}}>
+                <ToggleButton title="Hide terminated" checked={hideTerminated} onChange={setHideTerminated}/>
+            </div>
+            <TablePage
+                columns={columns}
+                url={APIS.ASSETS}
+                select="id,assetInfo,summary,lastSeen,terminatedOn"
+                tableTitle={TABLE_TITLE}
+                filterType={ASSETS_FILTER_TYPE}
+                filtersConfig={[
+                    ...getAssetColumnsFiltersConfig(),
+                    ...vulnerabilitiesCountersColumnsFiltersConfig,
+                    ...findingsColumnsFiltersConfig
+                ]}
+                filters={hideTerminated ? ["(terminatedOn eq null)"] : null}
+                defaultSortBy={{sortIds: ["lastSeen", "terminatedOn"], desc: true}}
+                withMargin
+            />
+        </div>
     )
 }
 
