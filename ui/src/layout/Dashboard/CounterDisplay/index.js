a435c81b8c7f09f4617e06effeeb07f40561106f
Tagscherer Ádám <adam.tagscherer@gmail.com>
2023-11-03 17:45:17 +0100

    fix(ui): calculate completed scans count correctly (vmclarity#898)
    * fix(scans-count): completed scans count
    
    * refactor(scans-count): create separate displays
    
    * refactor(scans-count): remove default export
    
    * refactor(scans-count): simplify logic
    
    * refactor(scans-count): simplify logic

--- ui/src/layout/Dashboard/CounterDisplay/index.js
+++ ui/src/layout/Dashboard/CounterDisplay/index.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import { useFetch } from 'hooks';
-import { formatNumber } from 'utils/utils';
+import React, {useMemo} from 'react';
+import {useFetch} from 'hooks';
+import {formatNumber} from 'utils/utils';
+
+import COLORS from "../../../utils/scss_variables.module.scss";
+import {APIS} from "../../../utils/systemConsts";
 
 import './counter-display.scss';
 
-const CounterDisplay = ({url, title, background}) => {
-    const [{data, error, loading}] = useFetch(url, {queryParams: {"$count": true, "$top": 1, "$select": "id"}});
-    
+export const ScanCounterDisplay = () => {
+    const [{data, error, loading}] = useFetch(
+        APIS.SCANS,
+        {
+            queryParams: {
+                "$count": true,
+                "$top": 1,
+                "$select": "id",
+                "$filter": "state eq 'Aborted' or state eq 'Failed' or state eq 'Done'",
+            }
+        }
+    );
+
     return (
-        <div className="dashboard-counter" style={{background}}>
-            {loading || error ? "" : 
-                <div className="dashboard-counter-content"><div className="dashboard-counter-count">{formatNumber(data.count)}</div>{` ${title}`}</div>
+        <div className="dashboard-counter" style={{background: COLORS["color-gradient-green"]}}>
+            {loading || error ? "" :
+                <div className="dashboard-counter-content">
+                    <div className="dashboard-counter-count">{formatNumber(data.count)}</div>
+                    Completed scans
+                </div>
             }
         </div>
     )
 }
 
-export default CounterDisplay;
+export const CounterDisplay = ({url, title, background}) => {
+    const [{data, error, loading}] = useFetch(url, {queryParams: {"$count": true, "$top": 1, "$select": "id"}});
+
+    return (
+        <div className="dashboard-counter" style={{background}}>
+            {loading || error ? "" :
+                <div className="dashboard-counter-content">
+                    <div className="dashboard-counter-count">{formatNumber(data.count)}</div>
+                    {` ${title}`}</div>
+            }
+        </div>
+    )
+}
