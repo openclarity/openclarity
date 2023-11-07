c51303aa1b4804a8e7d0839bdf138e8495d84d3e
Krisztian Gacsal <chrisgacsal@users.noreply.github.com>
2023-11-07 10:17:10 +0100

    vmclarity: ci: refactor release (vmclarity#906)
    * refactor: go binary build
    
    * ci: build vmclarity-cli distribution
    
    Add `dist-vmclarity-cli` target to Makefile for cross-compiling
    vmclarity-cli go binary and packaging them into tar archives which are
    used in CI as release artifacts.
    
    * ci: bundle Cloudformation artifact(s)
    
    * ci: bundle Azure Bicep manifest(s)
    
    * ci: bundle Docker Compose manifest
    
    * ci: bundle GCP Deployment manifest(s)
    
    * ci: bundle and publish Helm Chart
    
    * fix(helm): use valid semver in version field
    
    * ci: generate sha256sum for artifacts
    
    * ci: add changelog generation
    
    * ci: publish release
    
    * generate changelog
    * upload artifacts
    * switch to ncipollo/release-action publishing releases
    
    * ci: fix tar command for extracting tools

--- makefile.d/20-tools.mk
+++ makefile.d/20-tools.mk
@@ -100,3 +100,122 @@ bin/golangci-lint: bin/golangci-lint-$(GOLANGCI_VERSION)
 bin/golangci-lint-$(GOLANGCI_VERSION): | $(BIN_DIR)
 	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | bash -s -- -b "$(BIN_DIR)" "v$(GOLANGCI_VERSION)"
 	@mv bin/golangci-lint $@
+
+####
+##  yq CLI
+####
+
+YQ_BIN := $(BIN_DIR)/yq
+YQ_VERSION := 4.35.2
+
+bin/yq: bin/yq-$(YQ_VERSION)
+	@ln -sf $(notdir $<) $@
+
+bin/yq-$(YQ_VERSION): | $(BIN_DIR)
+	@curl -sSfL 'https://github.com/mikefarah/yq/releases/download/v$(YQ_VERSION)/yq_$(OSTYPE)_$(ARCHTYPE)' \
+	--output $@
+	@chmod +x $@
+
+####
+##  Helm CLI
+####
+
+HELM_BIN := $(BIN_DIR)/helm
+HELM_VERSION := 3.13.1
+
+bin/helm: bin/helm-$(HELM_VERSION)
+	@ln -sf $(notdir $<) $@
+
+bin/helm-$(HELM_VERSION): | $(BIN_DIR)
+	@curl -sSfL 'https://get.helm.sh/helm-v$(HELM_VERSION)-$(OSTYPE)-$(ARCHTYPE).tar.gz' --output - \
+	| tar xzvOf - '$(OSTYPE)-$(ARCHTYPE)/helm' > $@
+	@chmod +x $@
+
+####
+##  helm-docs CLI
+####
+
+HELMDOCS_BIN := $(BIN_DIR)/helm-docs
+HELMDOCS_VERSION := 1.11.3
+HELMDOCS_OSTYPE := $(OSTYPE)
+HELMDOCS_ARCH := $(ARCHTYPE)
+
+ifeq ($(HELMDOCS_OSTYPE),darwin)
+	HELMDOCS_OSTYPE = Darwin
+endif
+ifeq ($(HELMDOCS_OSTYPE),linux)
+	HELMDOCS_OSTYPE = Linux
+endif
+ifeq ($(HELMDOCS_ARCH),amd64)
+	HELMDOCS_ARCH = x86_64
+endif
+
+bin/helm-docs: bin/helm-docs-$(HELMDOCS_VERSION)
+	@ln -sf $(notdir $<) $@
+
+bin/helm-docs-$(HELMDOCS_VERSION): | $(BIN_DIR)
+	@curl -sSfL 'https://github.com/norwoodj/helm-docs/releases/download/v$(HELMDOCS_VERSION)/helm-docs_$(HELMDOCS_VERSION)_$(HELMDOCS_OSTYPE)_$(HELMDOCS_ARCH).tar.gz' --output - \
+	| tar xzvOf - 'helm-docs' > $@
+	@chmod +x $@
+
+####
+##  git-cliff CLI
+####
+
+GITCLIFF_BIN := $(BIN_DIR)/git-cliff
+GITCLIFF_VERSION := 1.4.0
+GITCLIFF_OSTYPE := $(OSTYPE)
+GITCLIFF_ARCH := $(ARCHTYPE)
+GITCLIFF_URL =
+
+ifeq ($(GITCLIFF_OSTYPE),darwin)
+	GITCLIFF_OSTYPE = apple-darwin
+endif
+ifeq ($(GITCLIFF_OSTYPE),linux)
+	GITCLIFF_OSTYPE = unknown-linux-gnu
+endif
+ifeq ($(GITCLIFF_ARCH),amd64)
+	GITCLIFF_ARCH = x86_64
+endif
+ifeq ($(GITCLIFF_ARCH),arm64)
+	GITCLIFF_ARCH = aarch64
+endif
+
+bin/git-cliff: bin/git-cliff-$(GITCLIFF_VERSION)
+	@ln -sf $(notdir $<) $@
+
+bin/git-cliff-$(GITCLIFF_VERSION): | $(BIN_DIR)
+	@curl -sSfL 'https://github.com/orhun/git-cliff/releases/download/v$(GITCLIFF_VERSION)/git-cliff-$(GITCLIFF_VERSION)-$(GITCLIFF_ARCH)-$(GITCLIFF_OSTYPE).tar.gz' --output - \
+	| tar xzvOf - 'git-cliff-$(GITCLIFF_VERSION)/git-cliff' > $@
+	@chmod +x $@
+
+####
+##  typos CLI
+####
+
+TYPOS_BIN := $(BIN_DIR)/typos
+TYPOS_VERSION := 1.4.0
+TYPOS_OSTYPE := $(OSTYPE)
+TYPOS_ARCH := $(ARCHTYPE)
+TYPOS_URL =
+
+ifeq ($(TYPOS_OSTYPE),darwin)
+	TYPOS_OSTYPE = apple-darwin
+endif
+ifeq ($(TYPOS_OSTYPE),linux)
+	TYPOS_OSTYPE = unknown-linux-gnu
+endif
+ifeq ($(TYPOS_ARCH),amd64)
+	TYPOS_ARCH = x86_64
+endif
+ifeq ($(TYPOS_ARCH),arm64)
+	TYPOS_ARCH = aarch64
+endif
+
+bin/typos: bin/typos-$(TYPOS_VERSION)
+	@ln -sf $(notdir $<) $@
+
+bin/typos-$(TYPOS_VERSION): | $(BIN_DIR)
+	@curl -sSfL 'https://github.com/crate-ci/typos/releases/download/v$(TYPOS_VERSION)/typos-v$(TYPOS_VERSION)-$(TYPOS_ARCH)-$(TYPOS_OSTYPE).tar.gz' --output - \
+	| tar xzvOf - './typos' > $@
+	@chmod +x $@
