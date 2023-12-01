423f24728ef46c8335a41bfb9fc373af483c0bca
Tamás Galló <tamasg@cisco.com>
2023-12-01 14:36:55 +0100

    feat(ui): use vite for building and bundling

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,7 +4,7 @@ import App from 'layout/App';
 
 import 'utils/fonts/fonts.scss';
 
-const container = document.getElementById('root');
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(<App />);
