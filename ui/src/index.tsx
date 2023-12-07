91c90be5861b42f96e9b7978f6f648486166e8b1
Tamás Galló <tamasg@cisco.com>
2023-12-07 14:30:47 +0100

    refactor(ui): switch to vite for building and bundling (#1012)
    * feat(ui): use vite for building and bundling
    
    * fix(ui): add browserslist support to vite config
    
    * fix(ui): add null check for root element

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -5,6 +5,9 @@ import App from 'layout/App';
 import 'utils/fonts/fonts.scss';
 
 const container = document.getElementById('root');
+if (container === null) {
+    throw new Error('Root element not found');
+}
 const root = createRoot(container);
 
 root.render(<App />);
