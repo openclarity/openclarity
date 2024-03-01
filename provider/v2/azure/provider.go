cd015cb7a815480db437a4a6ddd9d406e56a33d0
András Jáky <ajaky@cisco.com>
2024-03-01 09:13:22 +0100

    vmclarity: refactor: Azure provider v2 (vmclarity#1351)
    * refactor: implement Estimator in Azure providerv2
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * refactor: implement Kind method in Azure providerv2
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * refactor: implement Discoverer in Azure providerv2
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * refactor: RunAssetScan method in Azure providerv2
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * refactor: RemoveAssetScan method in Azure providerv2
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * refactor: move tests to Azure providerv2
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * fix: wrapcheck lint errors
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * refactor: common.go to utils, config.go to scanner package
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    * refactor: replace Scanner struct Config field
    
    Signed-off-by: András Jáky <ajaky@cisco.com>
    
    ---------
    
    Signed-off-by: András Jáky <ajaky@cisco.com>

--- provider/v2/azure/provider.go
+++ provider/v2/azure/provider.go
@@ -17,15 +17,17 @@ package azure
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v5"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v5"
 
 	apitypes "github.com/openclarity/vmclarity/api/types"
-	"github.com/openclarity/vmclarity/provider"
-	"github.com/openclarity/vmclarity/provider/v2/aws/discoverer"
-	"github.com/openclarity/vmclarity/provider/v2/aws/estimator"
-	"github.com/openclarity/vmclarity/provider/v2/aws/scanner"
+	"github.com/openclarity/vmclarity/provider/v2/azure/discoverer"
+	"github.com/openclarity/vmclarity/provider/v2/azure/estimator"
+	"github.com/openclarity/vmclarity/provider/v2/azure/scanner"
 )
-
-var _ provider.Provider = &Provider{}
 
 type Provider struct {
 	*discoverer.Discoverer
@@ -34,14 +36,61 @@ type Provider struct {
 }
 
 func (p *Provider) Kind() apitypes.CloudProvider {
-	// TODO implement me
-	panic("implement me")
+	return apitypes.Azure
 }
 
 func New(_ context.Context) (*Provider, error) {
+	config, err := NewConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load configuration: %w", err)
+	}
+
+	err = config.Validate()
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate configuration: %w", err)
+	}
+
+	cred, err := azidentity.NewManagedIdentityCredential(nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed create managed identity credential: %w", err)
+	}
+
+	networkClientFactory, err := armnetwork.NewClientFactory(config.SubscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create network client factory: %w", err)
+	}
+
+	computeClientFactory, err := armcompute.NewClientFactory(config.SubscriptionID, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create compute client factory: %w", err)
+	}
+
 	return &Provider{
-		Discoverer: &discoverer.Discoverer{},
-		Scanner:    &scanner.Scanner{},
-		Estimator:  &estimator.Estimator{},
+		Discoverer: &discoverer.Discoverer{
+			VMClient:    computeClientFactory.NewVirtualMachinesClient(),
+			DisksClient: computeClientFactory.NewDisksClient(),
+		},
+		Scanner: &scanner.Scanner{
+			Cred:             cred,
+			VMClient:         computeClientFactory.NewVirtualMachinesClient(),
+			SnapshotsClient:  computeClientFactory.NewSnapshotsClient(),
+			DisksClient:      computeClientFactory.NewDisksClient(),
+			InterfacesClient: networkClientFactory.NewInterfacesClient(),
+
+			SubscriptionID:              config.SubscriptionID,
+			ScannerLocation:             config.ScannerLocation,
+			ScannerResourceGroup:        config.ScannerResourceGroup,
+			ScannerSubnet:               config.ScannerSubnet,
+			ScannerPublicKey:            string(config.ScannerPublicKey),
+			ScannerVMSize:               config.ScannerVMSize,
+			ScannerImagePublisher:       config.ScannerImagePublisher,
+			ScannerImageOffer:           config.ScannerImageOffer,
+			ScannerImageSKU:             config.ScannerImageSKU,
+			ScannerImageVersion:         config.ScannerImageVersion,
+			ScannerSecurityGroup:        config.ScannerSecurityGroup,
+			ScannerStorageAccountName:   config.ScannerStorageAccountName,
+			ScannerStorageContainerName: config.ScannerStorageContainerName,
+		},
+		Estimator: &estimator.Estimator{},
 	}, nil
 }
--- provider/v2/azure/scanner/scanner.go
+++ provider/v2/azure/scanner/scanner.go
@@ -13,24 +13,143 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// nolint: wrapcheck
 package scanner
 
 import (
 	"context"
+	"fmt"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v5"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v5"
 
 	"github.com/openclarity/vmclarity/provider"
+	"github.com/openclarity/vmclarity/provider/v2/azure/utils"
 )
 
-var _ provider.Scanner = &Scanner{}
+const (
+	provisioningStateSucceeded = "Succeeded"
+	instanceIDPartsLength      = 9
+	resourceGroupPartIdx       = 4
+	vmNamePartIdx              = 8
+)
 
-type Scanner struct{}
+type Scanner struct {
+	Cred             azcore.TokenCredential
+	VMClient         *armcompute.VirtualMachinesClient
+	SnapshotsClient  *armcompute.SnapshotsClient
+	DisksClient      *armcompute.DisksClient
+	InterfacesClient *armnetwork.InterfacesClient
 
-func (s *Scanner) RunAssetScan(ctx context.Context, t *provider.ScanJobConfig) error {
-	// TODO implement me
-	panic("implement me")
+	SubscriptionID              string
+	ScannerLocation             string
+	ScannerResourceGroup        string
+	ScannerSubnet               string
+	ScannerPublicKey            string
+	ScannerVMSize               string
+	ScannerImagePublisher       string
+	ScannerImageOffer           string
+	ScannerImageSKU             string
+	ScannerImageVersion         string
+	ScannerSecurityGroup        string
+	ScannerStorageAccountName   string
+	ScannerStorageContainerName string
 }
 
-func (s *Scanner) RemoveAssetScan(ctx context.Context, t *provider.ScanJobConfig) error {
-	// TODO implement me
-	panic("implement me")
+// nolint:cyclop
+func (s *Scanner) RunAssetScan(ctx context.Context, config *provider.ScanJobConfig) error {
+	vmInfo, err := config.AssetInfo.AsVMInfo()
+	if err != nil {
+		return provider.FatalErrorf("unable to get vminfo from asset: %w", err)
+	}
+
+	resourceGroup, vmName, err := resourceGroupAndNameFromInstanceID(vmInfo.InstanceID)
+	if err != nil {
+		return err
+	}
+
+	assetVM, err := s.VMClient.Get(ctx, resourceGroup, vmName, nil)
+	if err != nil {
+		_, err = utils.HandleAzureRequestError(err, "getting asset virtual machine %s", vmName)
+		return err
+	}
+
+	snapshot, err := s.ensureSnapshotForVMRootVolume(ctx, config, assetVM.VirtualMachine)
+	if err != nil {
+		return fmt.Errorf("failed to ensure snapshot for vm root volume: %w", err)
+	}
+
+	var disk armcompute.Disk
+	if *assetVM.Location == s.ScannerLocation {
+		disk, err = s.ensureManagedDiskFromSnapshot(ctx, config, snapshot)
+		if err != nil {
+			return fmt.Errorf("failed to ensure managed disk created from snapshot: %w", err)
+		}
+	} else {
+		disk, err = s.ensureManagedDiskFromSnapshotInDifferentRegion(ctx, config, snapshot)
+		if err != nil {
+			return fmt.Errorf("failed to ensure managed disk from snapshot in different region: %w", err)
+		}
+	}
+
+	networkInterface, err := s.ensureNetworkInterface(ctx, config)
+	if err != nil {
+		return fmt.Errorf("failed to ensure scanner network interface: %w", err)
+	}
+
+	scannerVM, err := s.ensureScannerVirtualMachine(ctx, config, networkInterface)
+	if err != nil {
+		return fmt.Errorf("failed to ensure scanner virtual machine: %w", err)
+	}
+
+	err = s.ensureDiskAttachedToScannerVM(ctx, scannerVM, disk)
+	if err != nil {
+		return fmt.Errorf("failed to ensure asset disk is attached to virtual machine: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Scanner) RemoveAssetScan(ctx context.Context, config *provider.ScanJobConfig) error {
+	err := s.ensureScannerVirtualMachineDeleted(ctx, config)
+	if err != nil {
+		return fmt.Errorf("failed to ensure scanner virtual machine deleted: %w", err)
+	}
+
+	err = s.ensureNetworkInterfaceDeleted(ctx, config)
+	if err != nil {
+		return fmt.Errorf("failed to ensure network interface deleted: %w", err)
+	}
+
+	err = s.ensureTargetDiskDeleted(ctx, config)
+	if err != nil {
+		return fmt.Errorf("failed to ensure asset disk deleted: %w", err)
+	}
+
+	err = s.ensureBlobDeleted(ctx, config)
+	if err != nil {
+		return fmt.Errorf("failed to ensure snapshot copy blob deleted: %w", err)
+	}
+
+	err = s.ensureSnapshotDeleted(ctx, config)
+	if err != nil {
+		return fmt.Errorf("failed to ensure snapshot deleted: %w", err)
+	}
+
+	return nil
+}
+
+// Example Instance ID:
+//
+// /subscriptions/ecad88af-09d5-4725-8d80-906e51fddf02/resourceGroups/vmclarity-sambetts-dev/providers/Microsoft.Compute/virtualMachines/vmclarity-server
+//
+// Will return "vmclarity-sambetts-dev" and "vmclarity-server".
+func resourceGroupAndNameFromInstanceID(instanceID string) (string, string, error) {
+	idParts := strings.Split(instanceID, "/")
+	if len(idParts) != instanceIDPartsLength {
+		return "", "", provider.FatalErrorf("asset instance id in unexpected format got: %s", idParts)
+	}
+	return idParts[resourceGroupPartIdx], idParts[vmNamePartIdx], nil
 }
