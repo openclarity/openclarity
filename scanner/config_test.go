879eeb9fc999df408de7274401ca4f2426070428
Ramiz Polic <32913827+ramizpolic@users.noreply.github.com>
2024-07-17 12:48:21 +0200

    feat: add scanner config options (vmclarity#1928)

--- scanner/config_test.go
+++ scanner/config_test.go
@@ -19,18 +19,27 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
 	"github.com/openclarity/vmclarity/core/to"
 	"github.com/openclarity/vmclarity/scanner/common"
+	"github.com/openclarity/vmclarity/scanner/families"
+	exploits "github.com/openclarity/vmclarity/scanner/families/exploits/types"
+	infofinder "github.com/openclarity/vmclarity/scanner/families/infofinder/types"
 	malware "github.com/openclarity/vmclarity/scanner/families/malware/types"
+	misconfigurations "github.com/openclarity/vmclarity/scanner/families/misconfiguration/types"
+	plugins "github.com/openclarity/vmclarity/scanner/families/plugins/types"
+	rootkits "github.com/openclarity/vmclarity/scanner/families/rootkits/types"
 	sbom "github.com/openclarity/vmclarity/scanner/families/sbom/types"
 	secrets "github.com/openclarity/vmclarity/scanner/families/secrets/types"
 	vulnerabilities "github.com/openclarity/vmclarity/scanner/families/vulnerabilities/types"
 )
 
-func Test_ConfigAddInput(t *testing.T) {
+func Test_ConfigAddInputs(t *testing.T) {
 	type args struct {
-		mountPoints []string
-		config      *Config
+		inputs []string
+		config *Config
 	}
 	tests := []struct {
 		name string
@@ -40,7 +49,7 @@ func Test_ConfigAddInput(t *testing.T) {
 		{
 			name: "sbom, vuls, secrets and malware are enabled",
 			args: args{
-				mountPoints: []string{"/mnt/snapshot1"},
+				inputs: []string{"/mnt/snapshot1"},
 				config: &Config{
 					SBOM: sbom.Config{
 						Enabled: true,
@@ -100,7 +109,7 @@ func Test_ConfigAddInput(t *testing.T) {
 		{
 			name: "only vuls enabled",
 			args: args{
-				mountPoints: []string{"/mnt/snapshot1"},
+				inputs: []string{"/mnt/snapshot1"},
 				config: &Config{
 					Vulnerabilities: vulnerabilities.Config{
 						Enabled:       true,
@@ -125,10 +134,129 @@ func Test_ConfigAddInput(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.args.config.AddInputs(common.ROOTFS, tt.args.mountPoints)
+			tt.args.config.AddInputs(common.ROOTFS, tt.args.inputs)
 			got := tt.args.config
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("AddInputs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_NewConfig(t *testing.T) {
+	// Define shared config for mountpoints replacer option
+	mountpoints := []string{"/mount-a", "/mount-b"}
+	mountpointsInputsHave := []common.ScanInput{
+		{
+			Input:     "/dir-to-scan",
+			InputType: common.ROOTFS,
+		},
+		{
+			Input:     "nginx:latest",
+			InputType: common.IMAGE,
+		},
+	}
+	mountpointsInputsWant := []common.ScanInput{
+		{
+			Input:     "/mount-a/dir-to-scan",
+			InputType: common.ROOTFS,
+		},
+		{
+			Input:     "/mount-b/dir-to-scan",
+			InputType: common.ROOTFS,
+		},
+		{
+			Input:     "nginx:latest",
+			InputType: common.IMAGE,
+		},
+	}
+
+	tests := []struct {
+		name    string
+		options []ConfigOption
+		want    *Config
+	}{
+		{
+			name:    "no options",
+			options: nil,
+			want:    &Config{},
+		},
+		{
+			name: "base config",
+			options: []ConfigOption{
+				WithBaseConfig(Config{
+					SBOM: sbom.Config{
+						Enabled: true,
+					},
+				}),
+			},
+			want: &Config{
+				SBOM: sbom.Config{
+					Enabled: true,
+				},
+			},
+		},
+		{
+			name: "base config with sbom family replacer function",
+			options: []ConfigOption{
+				WithBaseConfig(Config{
+					SBOM: sbom.Config{
+						Enabled: true,
+						Inputs: []common.ScanInput{
+							{
+								Input: "",
+							},
+						},
+					},
+				}),
+				WithFamilyInputsReplacer(families.SBOM, func(input common.ScanInput) []common.ScanInput {
+					input.Input = "replaced"
+					return []common.ScanInput{input}
+				}),
+			},
+			want: &Config{
+				SBOM: sbom.Config{
+					Enabled: true,
+					Inputs: []common.ScanInput{{
+						Input: "replaced",
+					}},
+				},
+			},
+		},
+		{
+			name: "base config with mount points override",
+			options: []ConfigOption{
+				WithBaseConfig(Config{
+					SBOM:             sbom.Config{Inputs: mountpointsInputsHave},
+					Vulnerabilities:  vulnerabilities.Config{Inputs: mountpointsInputsHave},
+					Secrets:          secrets.Config{Inputs: mountpointsInputsHave},
+					Rootkits:         rootkits.Config{Inputs: mountpointsInputsHave},
+					Malware:          malware.Config{Inputs: mountpointsInputsHave},
+					Misconfiguration: misconfigurations.Config{Inputs: mountpointsInputsHave},
+					InfoFinder:       infofinder.Config{Inputs: mountpointsInputsHave},
+					Exploits:         exploits.Config{Inputs: mountpointsInputsHave},
+					Plugins:          plugins.Config{Inputs: mountpointsInputsHave},
+				}),
+				WithInputsMountOverride(mountpoints...),
+			},
+			want: &Config{
+				SBOM:             sbom.Config{Inputs: mountpointsInputsWant},
+				Vulnerabilities:  vulnerabilities.Config{Inputs: mountpointsInputsWant},
+				Secrets:          secrets.Config{Inputs: mountpointsInputsWant},
+				Rootkits:         rootkits.Config{Inputs: mountpointsInputsWant},
+				Malware:          malware.Config{Inputs: mountpointsInputsWant},
+				Misconfiguration: misconfigurations.Config{Inputs: mountpointsInputsWant},
+				InfoFinder:       infofinder.Config{Inputs: mountpointsInputsWant},
+				Exploits:         exploits.Config{Inputs: mountpointsInputsWant},
+				Plugins:          plugins.Config{Inputs: mountpointsInputsWant},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewConfig(tt.options...)
+			if !cmp.Equal(got, tt.want, cmpopts.EquateEmpty()) {
+				t.Errorf("NewConfig() = %v, want %v", got, tt.want)
 			}
 		})
 	}
