1ca8eac2bd4043330941b4742402822214a4e33e
Ramiz Polic <32913827+ramizpolic@users.noreply.github.com>
2024-08-12 12:26:40 +0200

    chore(general): cleanup deprecated kubeclarity code (#740)
    Signed-off-by: Ramiz Polic <ramiz.polic@hotmail.com>

--- kubeclarity/ui/src/components/Form/validators.js
+++
@@ -1,9 +0,0 @@
-import { isEmpty, isNumber } from 'lodash';
-
-export const validateRequired = value => (
-    isEmpty(value) && !isNumber(value) && value !== 0 ? "This field is required" : undefined
-);
-
-export const validateMaxScanField = value => (
-    value < 0 ? "Should be greater than or equal to 1" : undefined   
-);
--- kubeclarity/ui/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'layout/App';
-
-import 'utils/fonts/fonts.scss';
-
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
